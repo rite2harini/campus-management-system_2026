cfc71c30d66025350c3b072f89eae7e2e7373909
rite2harini <rite2harini@gmail.com>
2026-06-05 15:12:48 +0530

    Create marks.py

---
+++ modules/marks.py
@@ -0,0 +1,13 @@
+# =========================================
+# MODULE: MARKS SYSTEM
+# =========================================
+
+#  Purpose:
+# Handle student marks and grading
+
+# FUTURE FEATURES:
+# - Add marks
+# - Calculate grades
+# - Generate report cards
+
+# Placeholder for marks logic
