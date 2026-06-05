0e31e3255cbf922f927bab0b29e65382c6c87eb4
rite2harini <rite2harini@gmail.com>
2026-06-05 15:11:03 +0530

    Create students.py

---
+++ modules/students.py
@@ -0,0 +1,18 @@
+# =========================================
+# MODULE: STUDENT MANAGEMENT
+# =========================================
+
+#  Purpose:
+# This file will handle all student-related operations.
+
+# FUTURE FEATURES:
+# - Add student
+# - View students
+# - Search student
+# - Update student
+
+# NOTE:
+# Do NOT write full code now.
+# You will build this step-by-step in checkpoints.
+
+# Placeholder for student logic
