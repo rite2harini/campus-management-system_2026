6f86951b6d78638e9d35d595c1847d8c0e577292
rite2harini <rite2harini@gmail.com>
2026-06-05 15:09:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# =========================================
+# CAMPUS MANAGEMENT SYSTEM - MAIN FILE
+# =========================================
+
+# This is the starting point of your project.
+
+# You will slowly evolve this file into a full system.
+
+# FEATURES YOU WILL BUILD:
+# - Add Students
+# - Manage Courses
+# - Track Attendance
+# - Store Marks
+# - Generate Reports
+
+#  RULE:
+# Do NOT try to build everything at once.
+# Follow checkpoints step-by-step.
+
+print("Welcome to Campus Management System")
