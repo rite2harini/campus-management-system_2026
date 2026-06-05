3d9cd00687a74ef49100e2300efa8530f24d21e0
rite2harini <rite2harini@gmail.com>
2026-06-05 15:28:48 +0530

    Update checkpoint_3.py

--- checkpoints/checkpoint_3.py
+++ checkpoints/checkpoint_3.py
@@ -8,5 +8,17 @@
 # TASKS:
 # 1. Create add_student()
 # 2. Create view_students()
+#  BONUS CHALLENGE 1:
+# Create function:
+# search_student(name)
 
+#  BONUS CHALLENGE 2:
+# Create function:
+# delete_student(name)
+
+#  BONUS CHALLENGE 3:
+# Modify add_student() so it rejects empty input
+
+# BONUS CHALLENGE 4:
+# Why are functions better than writing everything in one block?
 print("Checkpoint 3 - Functions")
