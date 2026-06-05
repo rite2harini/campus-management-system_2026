f350f1c246fa6c6a21ab9e28021285d40c1b435e
rite2harini <rite2harini@gmail.com>
2026-06-05 15:14:18 +0530

    Create checkpoint_3.py

---
+++ checkpoints/checkpoint_3.py
@@ -0,0 +1,12 @@
+# =========================================
+# CHECKPOINT 3: FUNCTIONS INTRO
+# =========================================
+
+# GOAL:
+# Convert logic into functions
+
+# TASKS:
+# 1. Create add_student()
+# 2. Create view_students()
+
+print("Checkpoint 3 - Functions")
