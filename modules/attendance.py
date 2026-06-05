d461f199d9f669708b68c174826bb4c9d8314fa5
rite2harini <rite2harini@gmail.com>
2026-06-05 15:12:17 +0530

    Create attendance.py

---
+++ modules/attendance.py
@@ -0,0 +1,12 @@
+# =========================================
+# MODULE: ATTENDANCE SYSTEM
+# =========================================
+
+# Purpose:
+# Track student attendance
+
+# FUTURE FEATURES:
+# - Mark attendance
+# - View attendance report
+
+# Placeholder for attendance logic
