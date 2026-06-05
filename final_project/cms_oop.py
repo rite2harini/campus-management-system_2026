31ff2c1f2cadc2c9cb498b34a2f8870f2d60c548
rite2harini <rite2harini@gmail.com>
2026-06-05 15:15:25 +0530

    Create cms_oop.py

---
+++ final_project/cms_oop.py
@@ -0,0 +1,17 @@
+# =========================================
+# FINAL PROJECT: OOP VERSION
+# =========================================
+
+# GOAL:
+# Convert entire system into classes
+
+# FEATURES:
+# - Student class
+# - Course class
+# - Attendance class
+# - Marks class
+# - CampusSystem controller class
+
+# This is your industry-level final project
+
+print("Final OOP Project - Campus Management System")
