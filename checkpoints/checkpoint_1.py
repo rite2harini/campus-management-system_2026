26400ac0451a498d095a9136aba0bd0edad486e5
rite2harini <rite2harini@gmail.com>
2026-06-05 15:27:29 +0530

    Update checkpoint_1.py

--- checkpoints/checkpoint_1.py
+++ checkpoints/checkpoint_1.py
@@ -24,6 +24,19 @@
 #  RULES:
 # - Do NOT use loops
 # - Do NOT use functions
+## BONUS CHALLENGE 1:
+# Ask student for:
+# - name
+# - age
+# - department
+# Print a formatted profile like:
+# "Arun (18) from CSE department joined the system"
 
-#print("Checkpoint 1 - Start coding below ")
+# BONUS CHALLENGE 2:
+# Ask for 2 students and print both nicely formatted
+
+# BONUS CHALLENGE 3 (thinking task):
+# What happens if user enters a number as name?
+# Try and observe behavior
+print("Checkpoint 1 - Start coding below ")
 
