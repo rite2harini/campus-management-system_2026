0255a063ac102ab7be7b7fb9c47978619bd48ed1
rite2harini <rite2harini@gmail.com>
2026-06-05 15:13:18 +0530

    Create checkpoint_1.py

---
+++ checkpoints/checkpoint_1.py
@@ -0,0 +1,16 @@
+# =========================================
+# CHECKPOINT 1: BASIC SYSTEM START
+# =========================================
+
+#  GOAL:
+# Understand basic Python flow
+
+# TASKS:
+# 1. Print welcome message
+# 2. Take student name input
+# 3. Display greeting
+
+# RULE:
+# No functions, no lists yet
+
+print("Checkpoint 1 - Start Here")
